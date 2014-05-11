e932fabdf92d1d554cd447848ba94a0e8d276021
Sébastien Crozet <developer@crozet.re>
2014-05-11 11:06:28 +0200

    Temporary fix for the 2d ball-in-socket joint.
    Since nalgebra does not have staticly-sized rectangular matrices yet, the `col` operation required
    by the ball-in-socket joint torque axis is not available in 2d.
    
    This uses the `row` operation instead. To avoid breaking the 3d case, signs are inverted (the
    matrix from which we take the column is scew-symetric so: row(i) == -col(i)).

---
+++ examples/ragdoll2d.rs
@@ -0,0 +1,148 @@
+#![crate_type = "bin"]
+#![warn(non_camel_case_types)]
+
+extern crate std;
+extern crate native;
+extern crate rsfml;
+extern crate nalgebra;
+extern crate ncollide = "ncollide2df32";
+extern crate nphysics = "nphysics2df32";
+extern crate graphics2d;
+
+use std::rc::Rc;
+use std::cell::RefCell;
+use nalgebra::na::{Vec1, Vec2, Translation, Rotation};
+use ncollide::geom::{Plane, Cuboid, Ball};
+use nphysics::world::World;
+use nphysics::object::{RigidBody, Static, Dynamic};
+use nphysics::detection::joint::{Anchor, BallInSocket};
+use graphics2d::engine::GraphicsManager;
+
+#[start]
+fn start(argc: int, argv: **u8) -> int {
+    native::start(argc, argv, main)
+}
+
+fn main() {
+    GraphicsManager::simulate(boxes_vee_3d)
+}
+
+pub fn boxes_vee_3d(graphics: &mut GraphicsManager) -> World {
+    /*
+     * World
+     */
+    let mut world = World::new();
+    world.set_gravity(Vec2::new(0.0f32, 9.81));
+
+    /*
+     * A plane for the ground
+     */
+    let ground_geom = Plane::new(Vec2::new(0.0f32, -1.0));
+    let ground      = Rc::new(RefCell::new(RigidBody::new(ground_geom, 0.0f32, Static, 0.3, 0.6)));
+
+    world.add_body(ground.clone());
+    graphics.add(ground);
+
+    /*
+     * Create the ragdolls
+     */
+    let n     = 5;
+    let shift = 10.0;
+
+    for i in range(0, n) {
+        for j in range(0, n) {
+            let x = i as f32 * shift - n as f32 * shift / 2.0;
+            let y = j as f32 * (-shift) - 10.0;
+
+            add_ragdoll(Vec2::new(x, y), &mut world, graphics);
+        }
+    }
+
+    /*
+     * Set up the camera and that is it!
+     */
+
+    world
+}
+
+fn add_ragdoll(pos: Vec2<f32>, world: &mut World, graphics: &mut GraphicsManager) {
+    // head
+    let     head_geom = Ball::new(0.8);
+    let mut head      = RigidBody::new(head_geom, 1.0f32, Dynamic, 0.3, 0.5);
+    head.append_translation(&(pos + Vec2::new(0.0f32, -2.4)));
+
+    // body
+    let     body_geom = Cuboid::new(Vec2::new(1.2, 0.5));
+    let mut body      = RigidBody::new(body_geom, 1.0f32, Dynamic, 0.3, 0.5);
+    body.append_rotation(&-Vec1::new(Float::frac_pi_2()));
+    body.append_translation(&pos);
+
+    // right arm
+    let     rarm_geom = Cuboid::new(Vec2::new(1.6, 0.2));
+    let mut rarm      = RigidBody::new(rarm_geom, 1.0f32, Dynamic, 0.3, 0.5);
+    rarm.append_translation(&(pos + Vec2::new(2.4f32, -1.0)));
+
+    // left arm
+    let mut larm      = rarm.clone();
+    larm.append_translation(&Vec2::new(-4.8f32, 0.0));
+
+    // right foot
+    let     rfoot_geom = Cuboid::new(Vec2::new(1.6, 0.2));
+    let mut rfoot      = RigidBody::new(rfoot_geom, 1.0f32, Dynamic, 0.3, 0.5);
+    rfoot.append_rotation(&-Vec1::new(Float::frac_pi_2()));
+    rfoot.append_translation(&(pos + Vec2::new(0.4f32, 3.0)));
+
+    // left foot
+    let mut lfoot      = rfoot.clone();
+    lfoot.append_translation(&Vec2::new(-0.8f32, 0.0));
+
+
+    let head  = Rc::new(RefCell::new(head));
+    let body  = Rc::new(RefCell::new(body));
+    let rarm  = Rc::new(RefCell::new(rarm));
+    let larm  = Rc::new(RefCell::new(larm));
+    let rfoot = Rc::new(RefCell::new(rfoot));
+    let lfoot = Rc::new(RefCell::new(lfoot));
+
+    world.add_body(head.clone());
+    world.add_body(body.clone());
+    world.add_body(rarm.clone());
+    world.add_body(larm.clone());
+    world.add_body(rfoot.clone());
+    world.add_body(lfoot.clone());
+
+    // let color = graphics.gen_color();
+    graphics.add(head.clone());
+    graphics.add(body.clone());
+    graphics.add(rarm.clone());
+    graphics.add(larm.clone());
+    graphics.add(rfoot.clone());
+    graphics.add(lfoot.clone());
+
+    /*
+     * Create joints.
+     */
+    let body_anchor_head  = Anchor::new(Some(body.clone()), Vec2::new(1.3, 0.0));
+    let body_anchor_rarm  = Anchor::new(Some(body.clone()), Vec2::new(1.0, 0.75));
+    let body_anchor_larm  = Anchor::new(Some(body.clone()), Vec2::new(1.0, -0.75));
+    let body_anchor_rfoot = Anchor::new(Some(body.clone()), Vec2::new(-1.3, 0.2));
+    let body_anchor_lfoot = Anchor::new(Some(body.clone()), Vec2::new(-1.3, -0.2));
+
+    let head_anchor       = Anchor::new(Some(head), Vec2::new(0.0, 0.9));
+    let rarm_anchor       = Anchor::new(Some(rarm), Vec2::new(-1.5, 0.0));
+    let larm_anchor       = Anchor::new(Some(larm), Vec2::new(1.5, 0.0));
+    let rfoot_anchor      = Anchor::new(Some(rfoot), Vec2::new(1.5, 0.0));
+    let lfoot_anchor      = Anchor::new(Some(lfoot), Vec2::new(1.5, 0.0));
+
+    let head_joint  = BallInSocket::new(body_anchor_head,   head_anchor);
+    let rarm_joint  = BallInSocket::new(body_anchor_rarm,   rarm_anchor);
+    let larm_joint  = BallInSocket::new(body_anchor_larm,   larm_anchor);
+    let rfoot_joint = BallInSocket::new(body_anchor_rfoot, rfoot_anchor);
+    let lfoot_joint = BallInSocket::new(body_anchor_lfoot, lfoot_anchor);
+
+    world.add_ball_in_socket(Rc::new(RefCell::new(head_joint)));
+    world.add_ball_in_socket(Rc::new(RefCell::new(rarm_joint)));
+    world.add_ball_in_socket(Rc::new(RefCell::new(larm_joint)));
+    world.add_ball_in_socket(Rc::new(RefCell::new(rfoot_joint)));
+    world.add_ball_in_socket(Rc::new(RefCell::new(lfoot_joint)));
+}
--- src/resolution/constraint/ball_in_socket_equation.rs
+++ src/resolution/constraint/ball_in_socket_equation.rs
@@ -3,11 +3,17 @@ use std::cell::Ref;
 use nalgebra::na::{Row, Indexable};
 use nalgebra::na;
 use ncollide::math::{Scalar, Vect};
+use ncollide::math::Orientation;
 use object::RigidBody;
 use detection::joint::{Anchor, BallInSocket, Joint};
 use resolution::constraint::velocity_constraint::VelocityConstraint;
 use resolution::constraint::contact_equation::CorrectionParameters;
 use resolution::constraint::contact_equation;
+
+#[cfg(dim3)]
+use nalgebra::na::Mat3;
+#[cfg(dim2)]
+use nalgebra::na::Vec2;
 
 pub fn fill_second_order_equation(dt:          Scalar,
                                   joint:       &BallInSocket,
@@ -45,11 +51,25 @@ pub fn cancel_relative_linear_motion<P>(
         let opt_rb1 = write_anchor_id(anchor1, &mut constraint.id1);
         let opt_rb2 = write_anchor_id(anchor2, &mut constraint.id2);
 
-        // XXX: should be:
+        // XXX:
+        // We create this dimension-based dispatch.
+        // This is needed because nalgebra does not define the `column` operator for vectors.
+        // The true formula should be:
         // let rot_axis1 = -rot_axis1.col(i);
         // let rot_axis2 = rot_axis2.col(i);
-        let rot_axis1 = rot_axis1.row(i);
-        let rot_axis2 = -rot_axis2.row(i);
+        #[cfg(dim3)]
+        #[inline(always)]
+        fn rot_axis(rot_axis1: &Mat3<Scalar>, rot_axis2: &Mat3<Scalar>, i: uint) -> (Orientation, Orientation){
+            (rot_axis1.row(i), -rot_axis2.row(i))
+        }
+
+        #[cfg(dim2)]
+        #[inline(always)]
+        fn rot_axis(rot_axis1: &Vec2<Scalar>, rot_axis2: &Vec2<Scalar>, i: uint) -> (Orientation, Orientation) {
+            (-rot_axis1.row(i), rot_axis2.row(i))
+        }
+
+        let (rot_axis1, rot_axis2) = rot_axis(&rot_axis1, &rot_axis2, i);
 
         let dvel = contact_equation::relative_velocity(
             &opt_rb1.as_ref().map(|r| &**r),
