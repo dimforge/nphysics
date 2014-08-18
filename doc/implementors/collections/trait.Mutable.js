22a9b8e01b10b4b3907b3c2114dac7248ae77ae3
Sébastien Crozet <developer@crozet.re>
2014-08-18 22:11:32 +0200

    Update the doc.

---
+++ doc/implementors/collections/trait.Mutable.js
@@ -0,0 +1,13 @@
+(function() {var implementors = {};
+implementors['ncollide3df64'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;K, V, H: <a class='trait' href='ncollide3df64/data/hash/trait.HashFun.html' title='ncollide3df64::data::hash::HashFun'>HashFun</a>&lt;K&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/master/collections/trait.Mutable.html' title='collections::Mutable'>Mutable</a> for <a class='struct' href='ncollide3df64/data/hash_map/struct.HashMap.html' title='ncollide3df64::data::hash_map::HashMap'>HashMap</a>&lt;K, V, H&gt;",];
+implementors['ncollide2df32'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;K, V, H: <a class='trait' href='ncollide2df32/data/hash/trait.HashFun.html' title='ncollide2df32::data::hash::HashFun'>HashFun</a>&lt;K&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/master/collections/trait.Mutable.html' title='collections::Mutable'>Mutable</a> for <a class='struct' href='ncollide2df32/data/hash_map/struct.HashMap.html' title='ncollide2df32::data::hash_map::HashMap'>HashMap</a>&lt;K, V, H&gt;",];
+implementors['ncollide2df64'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;K, V, H: <a class='trait' href='ncollide2df64/data/hash/trait.HashFun.html' title='ncollide2df64::data::hash::HashFun'>HashFun</a>&lt;K&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/master/collections/trait.Mutable.html' title='collections::Mutable'>Mutable</a> for <a class='struct' href='ncollide2df64/data/hash_map/struct.HashMap.html' title='ncollide2df64::data::hash_map::HashMap'>HashMap</a>&lt;K, V, H&gt;",];
+implementors['ncollide3df32'] = ["<a class='stability Unmarked' title='No stability level'></a>impl&lt;K, V, H: <a class='trait' href='ncollide3df32/data/hash/trait.HashFun.html' title='ncollide3df32::data::hash::HashFun'>HashFun</a>&lt;K&gt;&gt; <a class='trait' href='http://doc.rust-lang.org/master/collections/trait.Mutable.html' title='collections::Mutable'>Mutable</a> for <a class='struct' href='ncollide3df32/data/hash_map/struct.HashMap.html' title='ncollide3df32::data::hash_map::HashMap'>HashMap</a>&lt;K, V, H&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
