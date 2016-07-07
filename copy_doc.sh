e0b7cb163380cb80602f8ba2b748fcda4b5a48f8
Sébastien Crozet <developer@crozet.re>
2016-07-07 23:15:06 +0200

    Update the script to copy the documentation.

--- copy_doc.sh
+++ copy_doc.sh
@@ -8,3 +8,4 @@ cd $nphysics_path; make doc
 cd -
 cp -r $nphysics_path/build/nphysics2d/target/doc doc
 cp -r $nphysics_path/build/nphysics3d/target/doc/nphysics3d doc
+cp -r $nphysics_path/build/nphysics3d/target/doc/src/nphysics3d doc/src
