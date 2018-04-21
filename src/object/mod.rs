f5ed97b6a95e3e82d936d0529268121f86d05b5c
Sébastien Crozet <sebastien@crozet.re>
2018-04-21 17:49:11 +0200

    Don't let ncollide compute contacts between static solids for nothing.

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -1,9 +1,5 @@
 //! Objects that may be added to the physical world.
 
-pub use self::rigid_body_collision_groups::RigidBodyCollisionGroups;
-pub use self::sensor_collision_groups::SensorCollisionGroups;
-pub use self::collision_groups_wrapper_impl::{SENSOR_GROUP_ID, STATIC_GROUP_ID};
-
 pub use self::material::Material;
 
 pub use self::ground::Ground;
@@ -14,10 +10,6 @@ pub use self::multibody_link::{MultibodyLink, MultibodyLinkId, MultibodyLinkMut,
 pub use self::rigid_body::RigidBody;
 pub use self::multibody::{Multibody, MultibodyLinks, MultibodyWorkspace};
 pub use self::collider::{Collider, ColliderData, ColliderHandle, Colliders, Sensor, SensorHandle};
-
-mod collision_groups_wrapper_impl;
-mod rigid_body_collision_groups;
-mod sensor_collision_groups;
 
 mod material;
 
