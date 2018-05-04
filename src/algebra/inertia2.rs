cd8053e3ec82f2b58db337971da49712ee8fc50a
Sébastien Crozet <sebastien@crozet.re>
2018-05-04 18:36:26 +0200

    Document the "algebra" module.

--- src/algebra/inertia2.rs
+++ src/algebra/inertia2.rs
@@ -4,39 +4,51 @@ use std::ops::{Add, AddAssign, Mul};
 use na::{self, Isometry2, Matrix1, Matrix3, Real, Vector3};
 use algebra::{Force2, Velocity2};
 
+/// The inertia of a rigid body grouping both its mass and its angular inertia.
 #[derive(Clone, Copy, Debug)]
 pub struct Inertia2<N: Real> {
+    /// The linear part (mass) of the inertia.
     pub linear: N,
+    /// The angular inertia.
     pub angular: N,
 }
 
 impl<N: Real> Inertia2<N> {
+    /// Creates an inertia from its linear and angular components.
     pub fn new(linear: N, angular: N) -> Self {
         Inertia2 { linear, angular }
     }
 
+    /// Creates an inertia from its linear and angular components.
     pub fn new_with_angular_matrix(linear: N, angular: Matrix1<N>) -> Self {
         Self::new(linear, angular.x)
     }
 
+    /// Get the mass.
     pub fn mass(&self) -> N {
         self.linear
     }
 
+    /// Create a zero inertia.
     pub fn zero() -> Self {
         Inertia2::new(na::zero(), na::zero())
     }
 
+    /// Get the angular inertia tensor.
     #[inline]
     pub fn angular_matrix(&self) -> &Matrix1<N> {
         unsafe { mem::transmute(&self.angular) }
     }
 
+    /// Convert the inertia into a matrix where the mass is represented as a 2x2
+    /// diagonal matrix on the upper-left corner, and the angular part as a 1x1
+    /// matrix on the lower-rigth corner.
     pub fn to_matrix(&self) -> Matrix3<N> {
         let diag = Vector3::new(self.linear, self.linear, self.angular);
         Matrix3::from_diagonal(&diag)
     }
 
+    /// Compute the inertia on the given coordinate frame.
     pub fn transformed(&self, _: &Isometry2<N>) -> Self {
         *self
     }
