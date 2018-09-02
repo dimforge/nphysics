6a51a42e02aecb82ef062aa6ff5024053c738ad4
sebcrozet <developer@crozet.re>
2018-09-02 19:43:47 +0200

    Add comments for future improvements.

--- nphysics_testbed2d/src/world_owner.rs
+++ nphysics_testbed2d/src/world_owner.rs
@@ -7,6 +7,8 @@ use std::sync::RwLock;
 /// This trait is designed to allow choosing implementation of underlying storing of
 /// World: shared between threads or with a single owner.
 pub trait WorldOwner {
+    // FIXME: avoid the systematic Box by using associated type
+    // which also requires the testbed to be parametrized by the world type.
     fn get<'a: 'b, 'b>(&'a self) -> Box<Deref<Target=World<f32>> + 'b>;
     fn get_mut<'a: 'b, 'b>(&'a mut self) -> Box<DerefMut<Target=World<f32>> + 'b>;
 }
